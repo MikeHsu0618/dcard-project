7d2c60c84e4581ef90d45910e19d7e581c152a69
Mike <a3698521tw11@gmail.com>
2022-02-20 23:35:59 +0800

    fix: optimize copy url
    - 優化 copy 功能

--- resources/asset/js/main.js
+++ resources/asset/js/main.js
@@ -41,12 +41,36 @@ const app = {
             }
         },
         copy() {
-            navigator.clipboard.writeText(this.shortUrl)
-                .then(() => {
-                    toastr['success']('已複製到剪貼簿', '成功')
-                }).catch(err => {
+            this.copyToClipboard()
+            .then(() => {
+                toastr['success']('已複製到剪貼簿', '成功')
+            }).catch(err => {
                 toastr['error']('好像哪裡出錯囉！ 請稍候重試～', '錯誤')
-            })
+                console.log(err)
+            });
+        },
+        copyToClipboard() {
+            if (navigator.clipboard && window.isSecureContext) {
+                // navigator clipboard 向剪贴板写文本
+                return navigator.clipboard.writeText(this.shortUrl)
+            } else {
+                // 创建text area
+                let textArea = document.createElement("textarea");
+                textArea.value = this.shortUrl;
+                // 使text area不在viewport，同时设置不可见
+                textArea.style.position = "absolute";
+                textArea.style.opacity = 0;
+                textArea.style.left = "-999999px";
+                textArea.style.top = "-999999px";
+                document.body.appendChild(textArea);
+                textArea.focus();
+                textArea.select();
+                return new Promise((res, rej) => {
+                    // 执行复制命令并移除文本框
+                    document.execCommand('copy') ? res() : rej();
+                    textArea.remove();
+                });
+            }
         },
         setToastr() {
             toastr.options = {
