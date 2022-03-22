1b0b5d18e6e39509abb4f959bf04847da75bd8b6
Mike <a3698521tw11@gmail.com>
2022-03-22 13:32:03 +0800

    feat: add swagger docs

---
+++ controller/api/v1/url.go
@@ -0,0 +1,121 @@
+package v1
+
+import (
+	"context"
+	. "dcard-project/database"
+	_ "dcard-project/docs"
+	"dcard-project/models"
+	"dcard-project/pkg/decimalconv"
+	"dcard-project/pkg/goquery"
+	"dcard-project/pkg/httputil"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+)
+
+const (
+	LockKey     = "lock_key"
+	BasicAmount = int64(20000)
+)
+
+var ctx = context.Background()
+
+// @Summary 產生短網址
+// @Description 請輸入合法原網址
+// @Tags Url
+// @Accept json
+// @Produce json
+// @Param url body models.createUrl true "Get Short Url"
+// @Success 200 {object} models.ApiUrl
+// @Router / [post]
+func CreateUrl(c *gin.Context) {
+	var url = &models.Url{}
+	var shortUrl string
+	// 接收參數
+	if err := c.ShouldBind(&url); err != nil {
+		httputil.NewError(c, 404, err.Error())
+		return
+	}
+	// 檢查原網址
+	res, err := http.Get(url.OrgUrl)
+	if err != nil || res.StatusCode != 200 {
+		httputil.NewError(c, 404, "invalid url")
+		return
+	}
+	meta := goquery.GetHtmlMeta(res.Body)
+
+	// 檢查是否已重複
+	result := Db.Create(url)
+	// 已存在則返回
+	if err := result.Error; err != nil && strings.Contains(err.Error(), "duplicate") {
+		duplicateUrl := &models.Url{}
+		Db.Where("org_url", url.OrgUrl).First(duplicateUrl)
+		shortUrl = decimalconv.Encode(BasicAmount + duplicateUrl.ID)
+		data := models.ApiUrl{
+			ShortUrl: shortUrl,
+			Meta:     meta,
+		}
+		httputil.NewSuccess(c, data)
+		return
+	}
+	if result.Error != nil {
+		httputil.NewError(c, 404, "create fail")
+		return
+	}
+
+	//產生縮網址
+	shortUrl = decimalconv.Encode(BasicAmount + url.ID)
+	data := models.ApiUrl{
+		ShortUrl: shortUrl,
+		Meta:     meta,
+	}
+	//保存三十天過期
+	if _, err := Redis.Set(
+		context.Background(),
+		strconv.FormatInt(url.ID, 10),
+		url.OrgUrl,
+		30*24*time.Hour).Result(); err != nil {
+		println("Redis Set Url Error", err.Error())
+		return
+	}
+
+	httputil.NewSuccess(c, data)
+}
+
+func ToOrgPage(c *gin.Context) {
+	var url = &models.Url{}
+	index := decimalconv.Decode(c.Param("shortUrl")) - BasicAmount
+	// 使用快取
+	if result, _ := Redis.Get(ctx, strconv.FormatInt(index, 10)).Result(); len(result) != 0 {
+		c.Redirect(http.StatusFound, result)
+		return
+	}
+
+	// 使用資料庫
+	for {
+		// 上鎖
+		if Lock(LockKey) == false {
+			time.Sleep(100 * time.Millisecond)
+			continue
+		}
+		Db.Where("id", index).First(&url)
+		if len(url.OrgUrl) == 0 {
+			UnLock(LockKey)
+			c.HTML(
+				http.StatusNotFound,
+				"404.html",
+				gin.H{"title": "無效的地址"},
+			)
+			return
+		}
+		Redis.Set(context.Background(), strconv.FormatInt(index, 10), url.OrgUrl, 30*24*time.Hour)
+		UnLock(LockKey)
+		break
+	}
+
+	// 保存三十天過期
+	Redis.Set(context.Background(), strconv.FormatInt(index, 10), url.OrgUrl, 30*24*time.Hour)
+	c.Redirect(http.StatusFound, url.OrgUrl)
+}
--- models/url.go
+++ models/url.go
@@ -1,29 +1,8 @@
 package models
 
 import (
-	"dcard-project/app/Logic/Goquery"
-	. "dcard-project/database"
-	"fmt"
+	"dcard-project/pkg/goquery"
 )
-
-// Input 本來就沒有質, 所以不用傳地址
-// create
-//models.Url{Name: "1111"}.Insert()
-
-// list Url{} 是值, 沒有地址
-//url := models.Url{}
-//urls, _ := url.Urls()
-//fmt.Printf("結果是 = %v", urls)
-
-// update
-//url := models.Url{Name: "太神拉"}
-//urls, _ := url.Update(666)
-//fmt.Printf("結果是 = %v", urls)
-
-// Destroy
-//url := models.Url{}
-//result, _ := url.Destroy(666)
-//fmt.Printf("結果是 = %v", result)
 
 type Url struct {
 	ID     int64  `json:"id" form:"id"`                              // 列名 `id`
@@ -31,69 +10,12 @@ type Url struct {
 }
 
 type ApiUrl struct {
-	ShortUrl string `json:"short_url" form:"short_url"`
-	Goquery.Meta
+	ShortUrl string `json:"short_url" form:"short_url" example:"5Cb"`
+	goquery.Meta
+}
+
+type createUrl struct {
+	OrgUrl string `json:"org_url" example:"https://www.google.com"`
 }
 
 var Urls []Url
-
-//添加
-func (url Url) Insert() (id int64, err error) {
-	fmt.Printf("這是 ID 值 %v", url)
-
-	//添加数据
-	result := Db.Create(&url)
-
-	id = url.ID
-	if result.Error != nil {
-		err = result.Error
-		return
-	}
-	return
-}
-
-//取得
-
-//列表
-func (url *Url) Urls() (urls []Url, err error) {
-	if err = Db.First(&urls).Error; err != nil {
-		return
-	}
-	return
-}
-
-func (url *Url) Count() (count int64, err error) {
-	if err = Db.Model(&url).Count(&count).Error; err != nil {
-		return
-	}
-	return
-}
-
-//修改
-func (url *Url) Update(id int64) (updateUrl Url, err error) {
-
-	if err = Db.Select([]string{"id", "org_url", "short_url"}).First(&updateUrl, id).Error; err != nil {
-		return
-	}
-
-	//参数1:是要修改的数据
-	//参数2:是修改的数据
-	if err = Db.Model(&updateUrl).Updates(&url).Error; err != nil {
-		return
-	}
-	return
-}
-
-// Destroy 删除数据
-func (url *Url) Destroy(id int64) (Result Url, err error) {
-
-	if err = Db.Select([]string{"id"}).First(&url, id).Error; err != nil {
-		return
-	}
-
-	if err = Db.Delete(&url).Error; err != nil {
-		return
-	}
-	Result = *url
-	return
-}
